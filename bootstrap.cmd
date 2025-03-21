055e02b49e9c59760eabdba955ccbc8c5694e098
Jonathan Melly <jonathan.melly@gmail.com>
2025-03-21 14:06:18 +0100

    fix(install): download ps1 if needed

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,2 +1,5 @@
 @ECHO off
+IF NOT EXIST ".\install.ps1" (
+    powershell.exe -Command Invoke-WebRequest -Uri https://github.com/ETML-INF/standard-toolset/raw/main/install.ps1 -OutFile .\install.ps1
+)
 powershell.exe -Command "& {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force; & '.\install.ps1'}"
