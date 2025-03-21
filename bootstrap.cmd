abc30719d90aad582154b9421496dfd0d0de23f2
Jonathan Melly <jonathan.melly@gmail.com>
2025-03-21 15:56:58 +0100

    fix(install): student cannot store script everywhere.. stores it in temp if needed

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,5 +1,13 @@
 @ECHO off
-IF NOT EXIST ".\install.ps1" (
-    powershell.exe -Command Invoke-WebRequest -Uri https://github.com/ETML-INF/standard-toolset/raw/main/install.ps1 -OutFile .\install.ps1
+setlocal enabledelayedexpansion
+SET "installScript=.\install.ps1"
+SET "tempScript=%TEMP%\install_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%.ps1"
+SET "url=https://github.com/ETML-INF/standard-toolset/raw/main/install.ps1"
+IF NOT EXIST "%installScript%" (
+    echo No local %installScript% found, downloading from %url% to %tempScript%
+    powershell.exe -Command Invoke-WebRequest -Uri "%url%" -OutFile "%tempScript%"
+    SET "installScript=%tempScript%"
+) ELSE (
+    echo Using local %installScript% script (from repo probably^)
 )
-powershell.exe -Command "& {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force; & '.\install.ps1'}"
+powershell.exe -Command "& {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force; & '!installScript!'}"
