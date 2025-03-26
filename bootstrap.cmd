9e25c14bea399522f18578a5ba6635746d6a62c0
Jonathan Melly <jonathan.melly@gmail.com>
2025-03-26 17:11:48 +0100

    fix(bootstrap): fix %% for script instead of command line

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,7 +1,7 @@
 @ECHO off
 setlocal enabledelayedexpansion
 SET "installScript=.\install.ps1"
-for /F "usebackq tokens=1" %i in (`powershell ^(get-date -format yyyy_MM_dd_H_mm_ss^)`) do set timestamp=%i
+for /F "usebackq tokens=1" %%i in (`powershell ^(get-date -format yyyy_MM_dd_H_mm_ss^)`) do set timestamp=%%i
 SET "tempScript=%TEMP%\install_%timestamp%.ps1"
 SET "url=https://github.com/ETML-INF/standard-toolset/raw/main/install.ps1"
 IF NOT EXIST "%installScript%" (
