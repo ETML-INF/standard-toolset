2737557f371a7bf52f32bb1178cad28ef0d80bd5
Jonathan Melly <jonathan.melly@gmail.com>
2025-07-07 08:25:41 +0200

    feat(install): nointeraction mode and error handling

--- install.ps1
+++ install.ps1
@@ -1,51 +1,70 @@
+param(
+    [Parameter(Mandatory=$false, HelpMessage="Disable user ability to chose folder")][string]$nointeraction=$false
+)
 Set-StrictMode -Version Latest
 
 $target_folder = "D:\data"
 $target_subfolder = "inf-toolset"
 $target_folder_alternative = "C:\$target_subfolder"
 
+try{
 
-# Check if the target folder exists
-if (Test-Path -Path $target_folder) {
-    # If the folder exists, set target to target_folder\target_subfolder
-    $target = Join-Path -Path $target_folder -ChildPath $target_subfolder
-    Write-Output "$target_folder folder exists. Target set to: $target"
-} else {
-    # If the folder doesn't exist, prompt the user
-    Write-Warning "$target_folder folder not found."
-    $userInput = Read-Host "Enter target folder path (press Enter for default: $target_folder_alternative)"
-    
-    # If user just pressed Enter, use the default path
-    if ([string]::IsNullOrEmpty($userInput)) {
-        $target = $target_folder_alternative
-        Write-Output "Using default path: $target"
+    # Check if the target folder exists
+    if (Test-Path -Path $target_folder) {
+	# If the folder exists, set target to target_folder\target_subfolder
+	$target = Join-Path -Path $target_folder -ChildPath $target_subfolder
+	Write-Output "$target_folder folder exists. Target set to: $target"
     } else {
-        # Otherwise use whatever the user entered
-        $target = $userInput
-        Write-Output "Using custom path: $target"
+	# If the folder doesn't exist, prompt the user
+	Write-Warning "$target_folder folder not found."
+	if ($nointeraction)
+	{
+	    $userInput=""
+	}
+	else{
+	    $userInput = Read-Host "Enter target folder path (press Enter for default: $target_folder_alternative)"
+	}
+	
+	# If user just pressed Enter/or no interaction, use the default alt path
+	if ([string]::IsNullOrEmpty($userInput)) {
+            $target = $target_folder_alternative
+            Write-Output "Using default alternative path: $target"
+	} else {
+            # Otherwise use whatever the user entered
+            $target = $userInput
+            Write-Output "Using custom path: $target"
+	}
     }
-}
 
-# Create target if needed
-Write-Output "Checking existence of final target folder: $target"
-if (-not (Test-Path -Path $target)) {
-    try {
-        New-Item -Path $target -ItemType Directory -ErrorAction Stop | Out-Null
-        Write-Output "Directory created successfully at: $target" -ForegroundColor Green
-    } catch {
-        Write-Error "Error creating directory: $_" -ForegroundColor Red
-        return
+    # Create target if needed
+    Write-Output "Checking existence of final target folder: $target"
+    if (-not (Test-Path -Path $target)) {
+	try {
+            New-Item -Path $target -ItemType Directory -ErrorAction Stop | Out-Null
+            Write-Output "Directory created successfully at: $target" -ForegroundColor Green
+	} catch {
+            Write-Error "Error creating directory: $_"
+            return
+	}
     }
-}
 
-# bootstrap.ps1 sets location to extracted archive directory
-# Rclone with archive content
-$scoopdirectory="$target\scoop"
-if (-not (Test-Path $scoopdirectory))
-{
-    New-Item -ItemType Directory -Path $scoopdirectory
-}
-& (Get-ChildItem -Path "scoop\apps\rclone\*\rclone.exe" | Select-Object -First 1).FullName --progress .\scoop $scoopdirectory
+    # bootstrap.ps1 sets location to extracted archive directory
+    # Rclone with archive content
+    $scoopdirectory="$target\scoop"
+    if (-not (Test-Path $scoopdirectory))
+    {
+	New-Item -ItemType Directory -Path $scoopdirectory
+    }
+    Write-Host "Syncing files"
+    & (Get-ChildItem -Path "scoop\apps\rclone\*\rclone.exe" | Select-Object -First 1).FullName --progress .\scoop $scoopdirectory
 
-Write-Output "Toolset install/update terminated on host"
-Write-Output "As path should be adapted in a user context, if needed, please run 'powershell $target\setup-user-env.ps1 from a user session..."
+    # Configure environment for current user (vscode context menu+shortcut) AND restore "current" junctions !!!
+    & .\setup-user-env.ps1 $target
+
+    Write-Host "Toolset install/update terminated on host" -ForegroundColor Green
+    Write-Warning "For other users, please run 'powershell $target\setup-user-env.ps1' to add desktop shortcut and setup PATH"
+
+}
+catch {
+    Write-Error "Error installing toolset: $_"
+}
